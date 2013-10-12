d84beef728667b6f0d0d433a74e30fbc14cfb80f
Gary Brunton <gbrunton@gmail.com>
2013-10-12 00:10:36 -0700

    Added the Publish command

---
+++ Publisher/Commands/Command.cs
@@ -0,0 +1,30 @@
+﻿using FubuCore.CommandLine;
+using StructureMap;
+using StructureMap.Graph;
+
+namespace Publisher.Commands
+{
+	public abstract class Command<TInputModel> : FubuCommand<TInputModel>, IFubuCommand
+	{
+		public override bool Execute(TInputModel inputModel)
+		{
+			ObjectFactory.Configure(config =>
+			{
+				config.Scan(scanner =>
+				{
+					scanner.TheCallingAssembly();
+					scanner.WithDefaultConventions();
+					scanner.RegisterConcreteTypesAgainstTheFirstInterface();
+					this.ManageScanner(scanner);
+				});
+				this.Config(config, inputModel);
+			});
+
+			return ((Command<TInputModel>)ObjectFactory.GetInstance(this.GetType())).ExecuteHydratedObject(inputModel);
+		}
+
+		protected virtual void Config(ConfigurationExpression config, TInputModel inputModel) { }
+		protected virtual void ManageScanner(IAssemblyScanner scanner) { }
+		protected abstract bool ExecuteHydratedObject(TInputModel inputModel);
+	}
+}
--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -1,9 +1,47 @@
-﻿namespace Publisher
+﻿using System;
+using FubuCore.CommandLine;
+
+namespace Publisher
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static int Main(string[] args)
 		{
+			if (args == null || args.Length == 0)
+			{
+				args = new []
+					{
+						"publish"
+					};
+			}
+
+			bool success;
+			try
+			{
+				var factory = new CommandFactory();
+				factory.SetAppName("ssaportal");
+				factory.RegisterCommands(typeof(IFubuCommand).Assembly);
+				factory.RegisterCommands(typeof(Program).Assembly);
+
+				var executor = new CommandExecutor(factory);
+				success = executor.Execute(args);
+			}
+			catch (CommandFailureException e)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("ERROR: " + e.Message);
+				Console.ResetColor();
+				success = false;
+			}
+			catch (Exception ex)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("ERROR: " + ex);
+				Console.ResetColor();
+				success = false;
+			}
+
+			return success ? 0 : 1;
 		}
 	}
 }
